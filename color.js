2d58ec750dd68d0a13da1bbd4353c0a508dd0df5
myoung1032 <63451120+myoung1032@users.noreply.github.com>
2023-02-18 22:46:33 -0500

    Create color.js

---
+++ color.js
@@ -0,0 +1,9 @@
+// Generated with https://paletton.com/#uid=75x0u0kigkU8ZuBdTpdmbh6rjc7
+export const colors = [
+  ['#FFB6C1', '#FFB6C1', '#FFB6C1', '#FFB6C1', '#FFB6C1'],
+  ['#FFB6C1', '#40E0D0', '#9D4452', '#9D4452', '#9D4452'],
+  ['#FFB6C1', '#F0E68C', '#40E0D0', '#9D4452', '#9D4452'],
+  ['#FFB6C1', '#9D4452', '#9D4452', '#3A7424', '#9D4452'],
+  ['#FFB6C1', '#9D4452', '#9D4452', '#9D4452', '#9D4452'],
+  ['#FFB6C1','#9D4452','#9D4452','#9D4452','#9D4452'],
+];
